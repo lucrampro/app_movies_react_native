e089031e42f808c28d2fd33d12aee8a6ee60ba8e
Moity <anthony.77x@gmail.com>
2021-02-26 13:49:16 +0100

    le commit d'une vie la suite avec resolution du Icon

--- src/navigation/Details.js
+++ src/navigation/Details.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image, View, StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Icon from "react-native-vector-icons/Icon";
+import { Icon } from "react-native-elements";
 // import Icon from 'react-native-vector-icons/Icon';
 
 export default function Details() {
